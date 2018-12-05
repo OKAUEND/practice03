487f45550981263038e3ac70d177b7505a2ea570
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2018-12-06 04:12:26 +0900

    バリデーションチェック処理を追加

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 $(function(){
-    const msg_text_max = '20文字以内で入力してください。';
-    const msg_empty = '入力必須です。';
-    const msg_email_type = 'emailの形式ではありません。';
-    const msg_textarea_max = '100文字以内で入力してください。';
+    const MSG_TEXT_MAX = '20文字以内で入力してください。';
+    const MSG_EMPTY = '入力必須です。';
+    const MSG_EMAIL_TYPE = 'emailの形式ではありません。';
+    const MSG_TEXTAREA_MAX = '100文字以内で入力してください。';
+    const MSG_NONE = "";
 
     $(".valid-text").keyup(function(){
         var form_group = $(this).closest('.form-group');
-
+        
         if($(this).val().length > 20)
         {
             form_group.removeClass('has-success').addClass('has-error');
-            form_group.find('.help-block').text(msg_text_max);
+            form_group.find('.help-block').text(MSG_TEXT_MAX);
         }
         else
         {
             form_group.removeClass('has-error').addClass('has-success');
-            form_group.find('.has-block').text('');
+            form_group.find('.help-block').text(MSG_NONE);
         }
     });
 
@@ -25,17 +26,17 @@ $(function(){
         if( $(this).val().length === 0)
         {
             form_group.removeClass('has-success').addClass('has-error');
-            form_group.find('.help-block').text(msg_empty);
+            form_group.find('.help-block').text(MSG_EMPTY);
         }
-        else if($(this).val().length > 50 || !$(this.val().match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-]+)*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/)))
+        else if($(this).val().length > 50 || !$(this).val().match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/))
         {
             form_group.removeClass('has-success').addClass('has-error');
-            form_group.find('.help-block').text(msg_email_type);
+            form_group.find('.help-block').text(MSG_EMAIL_TYPE);
         }
         else
         {
             form_group.removeClass('has-error').addClass('has-success');
-            form_group.find('.has-block').text('');
+            form_group.find('.help-block').text(MSG_NONE);
         }
     });
 
@@ -45,17 +46,44 @@ $(function(){
       if( $(this).val().length === 0)
       {
           form_group.removeClass('has-success').addClass('has-error');
-          form_group.find('.help-block').text(msg_empty);
+          form_group.find('.help-block').text(MSG_EMPTY);
       }
       else if($(this).val().length > 100)
       {
           form_group.removeClass('has-success').addClass('has-error');
-          form_group.find('.help-block').text(msg_textarea_max);
+          form_group.find('.help-block').text(MSG_TEXTAREA_MAX);
       }
       else
       {
           form_group.removeClass('has-error').addClass('has-success');
-          form_group.find('.has-block').text('');
+          form_group.find('.help-block').text(MSG_NONE);
       }
+    });
+
+    $(".format-number").change(function()
+    {
+        var format_before = $(this).val();
+
+        format_before = format_before.replace("-","");
+
+        var format_after = format_before.replace(/[Ａ-Ｚａ-ｚ０-９]/g,function(s)
+        {
+            return string.fromCharCode(s.charCodeAt(0)-0xFEE0)
+        });
+
+        if(format_after.match(/^[A-Za-z]/))
+        {
+            alert("0~9の数字を入力してください.")
+            return;
+        }
+
+        if(format_after.length === 11)
+        {
+            $(this).val(format_after.substr(0,3)+'-'+format_after.substr(3,4)+'-'+format_after.substr(7,4));
+        }
+        else
+        {
+            $(this).val(format_after);
+        }
     });
 });
